e6ce1dbc602e63e9fea1ca1f3baefb285927084f
nasuda <nasuda908@gmail.com>
2026-04-04 14:56:44 +0900

    add func

--- first/main.mjs
+++ first/main.mjs
@@ -1,9 +1,13 @@
 const args = process.argv.slice(2);
+function write() {
+  process.stdout.write("test stdout\n");
+}
 
 function main() {
 
   console.log("main");
   console.log(args);
+  write();
 }
 
 main();
