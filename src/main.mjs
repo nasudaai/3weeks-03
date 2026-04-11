f943229f6b9166ccf32e7afee3e47a7419f7e489
nasuda <nasuda908@gmail.com>
2026-04-12 05:54:03 +0900

    add write func

--- src/main.mjs
+++ src/main.mjs
@@ -5,15 +5,18 @@ const args = argv.slice(2);
 console.log(args)
 
 
-function write() {
+function write(notString) {
+  
+  const json = JSON.stringify(notString);
   process.stdout.write("test\n");
+  process.stdout.write(json + "\n");
 }
 
 function main() {
-  write();
+  write("string");
 }
 
-write();
+//write();
 
 
 main();
