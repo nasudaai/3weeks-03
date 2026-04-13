1d3df006c453c52e0d3d4d8c2a32fb8aa9ec264d
nasuda <nasuda908@gmail.com>
2026-04-13 16:36:41 +0900

    add

--- third/main.mjs
+++ third/main.mjs
@@ -2,3 +2,12 @@ const args = process.argv.slice(2);
 console.log("test");
 
 console.log(args);
+
+function reJson(arg) {
+  
+  const json = JSON.stringify(arg);
+  return json;
+}
+
+const json = reJson(args);
+console.log(json);
