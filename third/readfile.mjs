c3576c062a6db139c4e03e6ccb3bacbbcebe0285
nasuda <nasuda908@gmail.com>
2026-04-18 12:35:41 +0900

    add buf and string

--- third/readfile.mjs
+++ third/readfile.mjs
@@ -2,5 +2,8 @@ import { readFileSync } from 'node:fs';
 
 console.log(readFileSync);
 
-const data = readFileSync("./sample.md");
-console.log(data);
+const buffer = readFileSync("./sample.md");
+console.log(buffer)
+
+const txt = readFileSync("./sample.md", "utf-8")
+console.log(txt);
